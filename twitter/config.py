3779e0998d8c277a0212c4affd3b300ea382ead9
alpeshrpatel <alpeshrpatel@gmail.com>
2018-09-03 13:26:45 -0700

    remove key

--- twitter/config.py
+++ twitter/config.py
@@ -4,7 +4,4 @@
 #	https://apps.twitter.com/app/new
 #--------------------------------------------------------------------------------
 
-consumer_key = "tmiiFOB19YiWMUnLnK6B7HlBk"
-consumer_secret = "Ng1LT4LKJdgj8b3zNtf6iHcRvbnmxr3gpNIoHBVnD6uxax0Vqu"
-access_key = "1018303920857202690-3KpgpeqcssaeoFIKJuZt9HDXsA2VNC"
-access_secret = "euA7MwBM1gtbomaMf5fw0aWOpg3rALvBv8jU72dBULmDT"
+
