450fe1060227ea1030950241fef14e28fb30e8a0
alpeshrpatel <alpeshrpatel@gmail.com>
2018-09-08 21:27:19 -0700

    added pymongo in news

--- news/feeparser.py
+++ news/feeparser.py
@@ -1,4 +1,17 @@
 import feedparser
+import pymongo
+from pymongo import MongoClient
+
+connection = pymongo.MongoClient("mongodb://localhost")
+
+
+# List of RSS feeds that we will fetch and combine
+newsurls = {
+    'apnews':           'http://hosted2.ap.org/atom/APDEFAULT/3d281c11a96b4ad082fe88aa0db04305',
+    'googlenews':       'https://news.google.com/news/rss/?hl=en&amp;ned=us&amp;gl=US',
+    'yahoonews':        'http://news.yahoo.com/rss/'
+}
+ 
  
 # Function to fetch the rss feed and return the parsed RSS
 def parseRSS( rss_url ):
@@ -14,25 +27,35 @@ def getHeadlines( rss_url ):
     
     return headlines
  
-# A list to hold all headlines
-allheadlines = []
- 
-# List of RSS feeds that we will fetch and combine
-newsurls = {
-    'apnews':           'http://hosted2.ap.org/atom/APDEFAULT/3d281c11a96b4ad082fe88aa0db04305',
-    'googlenews':       'https://news.google.com/news/rss/?hl=en&amp;ned=us&amp;gl=US',
-    'yahoonews':        'http://news.yahoo.com/rss/'
-}
- 
-# Iterate over the feed urls
-for key,url in newsurls.items():
-    # Call getHeadlines() and combine the returned headlines with allheadlines
-    allheadlines.extend( getHeadlines( url ) )
- 
- 
-# Iterate over the allheadlines list and print each headline
-for hl in allheadlines:
-    print(hl)
+
+def readNewsURL( ):
+    # A list to hold all headlines
+    allheadlines = []    
+    # Iterate over the feed urls
+    for key,url in newsurls.items():
+        # Call getHeadlines() and combine the returned headlines with allheadlines
+        allheadlines.extend( getHeadlines( url ) )
+    # Iterate over the allheadlines list and print each headline
+    # for hl in allheadlines:
+    #     print(hl)
+    insertInToMongo(allheadlines)
+    return
+
+
+
+def insertInToMongo(allheadlines):
+    # get a handle to the reddit database
+    db = connection.news
+    news = db.news
+    for hl in allheadlines:
+        # print(hl)
+        news.insert_one({"title":hl})
+    # put it in mongo
+    return
+            
+
+if __name__ == "__main__":
+   readNewsURL()    
  
  
 # end of code 
